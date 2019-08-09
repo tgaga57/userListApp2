25f3575e8ff30b6c591ca272eb68d09793d7dba7
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-08-09 23:43:37 +0900

    first commit

---
+++ useListApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  useListApp2
+//
+//  Created by 志賀大河 on 2019/08/09.
+//  Copyright © 2019 Taigashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
